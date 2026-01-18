d14f116b696ec22cf2a5500eaefae829602b7f5e
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-01-18 10:11:26 -0300

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'bbraun-escala-v4.9';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap'
+];
+
+// Instalação e Cache
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Estratégia: Tenta Rede, se falhar, usa Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
